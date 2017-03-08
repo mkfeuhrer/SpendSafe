a61cef060442ce7b9f21ffe42deb226d49ecf8ab
mkfeuhrer <mohitfeuhrer@gmail.com>
2017-03-08 17:09:39 +0530

    Adding horizontal bar chart , and minus database and updated piechart display

--- app/src/main/java/com/example/mohit/spendsafe/home.java
+++ app/src/main/java/com/example/mohit/spendsafe/home.java
@@ -1,10 +1,10 @@
 package com.example.mohit.spendsafe;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,34 +15,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.formatter.YAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.ViewPortHandler;
-
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-
-import static android.R.attr.data;
-import static android.R.attr.entries;
 
 public class home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    Double sum_income=0.0,sum_expense=0.0;
     PieChart pieChart ;
-    ArrayList<Entry> entries ;
-    ArrayList<String> PieEntryLabels ;
-    PieDataSet pieDataSet ;
+    ArrayList<Entry> entries;
+    ArrayList<BarEntry> entries_bar;
+    ArrayList<String> PieEntryLabels,BarEntryLables ;
+    PieDataSet pieDataSet;
     PieData pieData ;
     Button add,minus;
-    int k=1;
+    int k=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,14 +48,14 @@ public class home extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -68,14 +66,59 @@ public class home extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         pieChart = (PieChart) findViewById(R.id.chart1);
-
         entries = new ArrayList<>();
-
+        entries_bar = new ArrayList<>();
+        BarEntryLables = new ArrayList<String>();
         PieEntryLabels = new ArrayList<String>();
+        String inputval="0.0";
+        AddValuesToPIEENTRY("",inputval);
 
-        float inputval=0.0f;
-        AddValuesToPIEENTRY(inputval,"");
-
+        add = (Button)findViewById(R.id.add);
+        minus = (Button)findViewById(R.id.minus);
+        //database fetch
+        SQLiteDatabase data=openOrCreateDatabase("SpendSafe",MODE_PRIVATE,null);
+        data.execSQL("create table if not exists add_money (amount varchar,description varchar,date varchar,time varchar);");
+        data.execSQL("create table if not exists minus_money (category varchar primary key,amount varchar,description varchar,date varchar,time varchar);");
+        String income_fetch = "select amount from add_money";
+        Cursor income = data.rawQuery(income_fetch,null);
+        if(income.moveToFirst())
+        {
+            do{
+                String tmp_income;
+                tmp_income = income.getString(0);
+                //System.out.println(tmp_income);
+                Double amt = Double.parseDouble(tmp_income);
+                sum_income+=amt;
+            }while(income.moveToNext());
+        }
+        String category_fetch = " SELECT category,amount from minus_money";
+        Cursor cursor = data.rawQuery(category_fetch, null);
+        if(cursor.moveToFirst())
+        {
+            do{
+                String tmp_cat,tmp_amt;
+                tmp_cat = cursor.getString(0);
+                tmp_amt = cursor.getString(1);
+                Double amt = Double.parseDouble(tmp_amt);
+                sum_expense+=amt;
+                System.out.println(tmp_amt);
+                AddValuesToPIEENTRY(tmp_cat,tmp_amt);
+            }while(cursor.moveToNext());
+        }
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(home.this,addmoney.class);
+                startActivity(i);
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i = new Intent(home.this,minusmoney.class);
+                startActivity(i);
+            }
+        });
         pieDataSet = new PieDataSet(entries, "");
         pieDataSet.setValueTextColor(Color.BLACK);
         pieDataSet.setValueTextSize(15f);
@@ -85,25 +128,42 @@ public class home extends AppCompatActivity
         pieChart.animateY(3000);
         pieChart.getLegend().setEnabled(false);
         pieChart.setDescription("");
-        add = (Button)findViewById(R.id.add);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(home.this,addmoney.class);
-                startActivity(i);
-            }
-        });
 
         // pieChart.saveToGallery("mychart.jpg", 85);
 
+
+        HorizontalBarChart barChart = (HorizontalBarChart) findViewById(R.id.barchart);
+        ArrayList<BarEntry> bargroup1 = new ArrayList<>();
+        Float sum_exp = Float.parseFloat(sum_expense.toString());
+        Float sum_inc = Float.parseFloat(sum_income.toString());
+        System.out.println(sum_exp+" "+sum_inc);
+        entries_bar.add(new BarEntry(sum_exp, 1));
+        entries_bar.add(new BarEntry(sum_inc, 0));
+        BarDataSet bardataset = new BarDataSet(entries_bar, "Cells");
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Income");
+        labels.add("Expense");
+        BarData data1 = new BarData(labels, bardataset);
+        barChart.setData(data1);
+        bardataset.setColors(ColorTemplate.PASTEL_COLORS);
+        //YAxisValueFormatter custom = new MyYAxisValueFormatter();
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis =barChart.getAxisRight();
+        rightAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+
     }
 
-    public void AddValuesToPIEENTRY(float f,String data){
-
+    public void AddValuesToPIEENTRY(String data,String amt){
+        Float f = Float.parseFloat(amt);
         entries.add(new BarEntry(f, k));
         PieEntryLabels.add(data);
         k++;
     }
+
 
     @Override
     public void onBackPressed() {
